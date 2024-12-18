857bb68fcc5d1778eea2279ac056872a2a9e846b
Farriel Arrianta <farrielarrianta@gmail.com>
2024-12-18 22:04:10 +0700

    add /events to api.php

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::apiResource('events', EventController::class);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->get('/events', [EventController::class, 'index']);
